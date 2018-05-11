000187c31508f3de15d2062e12fb8926bafe0fe1
Michael Liendo <model3volution@gmail.com>
2018-05-10 22:38:29 -0500

    feat: setup default meetup endpoint

--- services/meetup.js
+++ services/meetup.js
@@ -1,1 +1,32 @@
 const axios = require('axios')
+
+const URL = process.env.MEETUP_ENDPOINT
+
+const NEXT_EVENT_DEFAULTS = {
+  name: 'Not yet assigned',
+  yes_rsvp_count:0,
+  time: new Date.now()
+}
+const parseInfo = (meetupJSON = {}) => {
+  const {
+    timezone = 'US/Central', 
+    next_event = NEXT_EVENT_DEFAULTS
+  } = meetupJSON
+
+  return {
+    timezone,
+    next_event
+  }
+}
+
+const getNextMeetup = () => {
+  return new Promise((resolve, reject) => {
+    return axios.get(url)
+    .then(res => res.data)
+    .then(parseInfo)
+    .then(nextMeetupInfo => resolve(nextMeetupInfo))
+    .catch(e => reject('error: ', e.message))
+  })
+}
+
+module.exports.getNextMeetup = getNextMeetup
