0c011fc7a85f77bbf0cfcae088f5e3c260048e16
Michael Liendo <model3volution@gmail.com>
2018-05-10 23:01:10 -0500

    feat: server stood up

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 require('dotenv').config()
-
+const express = require('express')
 const getNextMeetup = require('./services/meetup').getNextMeetup
+const bodyParser = require('body-parser')
 
-getNextMeetup()
-.then(data => console.log(data))
-.catch(e => console.log(e))
+const PORT = process.env.PORT
+const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post('/meetup/next', (req,res) => {
+  console.log(req.body)
+  getNextMeetup()
+  .then(data => {
+    res.status(200).json({})
+  })
+  .catch(e => console.log(e))
+})
+
+app.listen(PORT, () => console.log(`express server listening on port: ${PORT}`))
