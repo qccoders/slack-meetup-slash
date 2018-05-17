4df1e271d64fe989886c55e80e54dc9a4f4c556b
Michael Liendo <model3volution@gmail.com>
2018-05-16 23:33:48 -0500

    chore: remove dead code

--- services/meetup.js
+++ services/meetup.js
@@ -2,23 +2,13 @@ const format = require('date-fns').format
 const fetch = require('./qccoders-fetch')
 const URL = process.env.MEETUP_ENDPOINT
 
-const NEXT_EVENT_DEFAULTS = {
-  name: 'Not yet assigned',
-  yes_rsvp_count:0,
-  time: undefined
-}
-
-const formatMeetupMessage = (meetupInfo) => {
+const formatRSVPMessage = (meetupInfo) => {
   return `Currently, there are ${meetupInfo.next_event.yes_rsvp_count} people going.`
 }
 
-const parseInfo = (meetupJSON = {}) => {
-  const {
-    timezone = 'US/Central', 
-    next_event = NEXT_EVENT_DEFAULTS
-  } = meetupJSON
+const parseInfo = (meetupJSON) => {
   
-  const message = formatMeetupMessage({timezone, next_event})
+  const message = formatRSVPMessage({timezone, next_event})
   const nextMeetupLink = `${meetupJSON.link}events/${meetupJSON.next_event.id}`
 
   return {message, nextMeetupLink}
